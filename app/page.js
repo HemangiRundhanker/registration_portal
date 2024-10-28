1756f9be30ec01d643b7f56b927a5731a80e535b
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-10-28 16:39:12 +0530

    save

--- app/page.js
+++ app/page.js
@@ -20,23 +20,20 @@ export default function Home() {
   }, []);
   console.log(screenWidth);
   useEffect(() => {
-    session &&
-      router.push(
-        `/LandingPage`
-      );
+    session && router.push(`/LandingPage`);
   }, [session]);
   return (
     <>
       <BackgroundVideo />
       <section className={styles.header}>
-        <h1>PARUL HACK-VERSE</h1>
+        <h1>BYTE QUIZ</h1>
       </section>
       <section className={styles.card}>
         <div className={styles.textData}>
           <div className={styles.details}>
             <h1>Registrations Open</h1>
             <div className={styles.specifics}>
-              <h2>Till 25th October</h2>
+              <h2>Till 5th November 8 pm</h2>
               <p>
                 <span>
                   <svg
@@ -48,7 +45,7 @@ export default function Home() {
                     <path d="M19.5 4h-3V2.5a.5.5 0 00-1 0V4h-7V2.5a.5.5 0 00-1 0V4h-3A2.503 2.503 0 002 6.5v13A2.503 2.503 0 004.5 22h15a2.502 2.502 0 002.5-2.5v-13A2.502 2.502 0 0019.5 4zM21 19.5c0 .828-.672 1.5-1.5 1.5h-15C3.67 21 3 20.328 3 19.5V11h18v8.5zm0-9.5H3V6.5C3 5.672 3.67 5 4.5 5h3v1.5a.5.5 0 001 0V5h7v1.5a.5.5 0 001 0V5h3c.828 0 1.5.672 1.5 1.5V10z" />
                   </svg>
                 </span>
-                3-4 January 2025
+                6 November 2024
               </p>
               <p>
                 <span>
@@ -61,7 +58,7 @@ export default function Home() {
                     <path d="M250 100c69.333 0 128.333 24.333 177 73s73 107.667 73 177c0 70.667-20.667 151.667-62 243s-83.333 165.667-126 223l-62 84c-6.667-8-15.667-19.667-27-35-11.333-15.333-31.333-45-60-89s-54-87.333-76-130-42-91.667-60-147S0 394 0 350c0-69.333 24.333-128.333 73-177s107.667-73 177-73m0 388c37.333 0 69.333-13.333 96-40s40-58.667 40-96-13.333-69-40-95-58.667-39-96-39-69 13-95 39-39 57.667-39 95 13 69.333 39 96 57.667 40 95 40" />
                   </svg>
                 </span>
-                Parul University, Vadodara
+                205, A Block, GD Goenka University
               </p>
               <p>
                 <span>
@@ -79,21 +76,40 @@ export default function Home() {
                     <path d="M12 6v6l4 2" />
                   </svg>
                 </span>
-                48 hour Hackathon
+                Starts at 10 am
               </p>
-              <div className={styles.prize}>
-                <img src="/images/trophy.png" alt="" />
-                <div className={styles.prizeDetails}>
-                  <h5>Prize Pool Worth</h5>
-                  <h5>6.45 Lakh rupees</h5>
-                </div>
+              <p>
+                <span>
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    viewBox="0 0 24 24"
+                    height="1.3rem"
+                    width="1.3rem"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <path d="M4 4h6v6H4zM14 4h6v6h-6zM4 14h6v6H4z" />
+                    <path d="M20 17 A3 3 0 0 1 17 20 A3 3 0 0 1 14 17 A3 3 0 0 1 20 17 z" />
+                  </svg>
+                </span>
+                Offline
+              </p>
+            </div>
+            <div className={styles.prize}>
+              <img src="/images/trophy.png" alt="" />
+              <div className={styles.prizeDetails}>
+                <h5>Exciting Winning Prizes</h5>
+                {/* <h5>6.45 Lakh rupees</h5> */}
               </div>
             </div>
             <section className={styles.logos}>
-              <img src="/images/GKM_logo.png" alt="" />
-              <img src="/images/Nexium_logo.png" alt="" />
+              {/* <img src="/images/GKM_logo.png" alt="" /> */}
+              <img src="/images/byteLogo.png" alt="" />
               <img src="/images/RYM_logo.png" alt="" />
-              <img src="/images/University_logo.png" alt="" />
+              {/* <img src="/images/University_logo.png" alt="" /> */}
             </section>
           </div>
           <div className={styles.registerbtn}>
